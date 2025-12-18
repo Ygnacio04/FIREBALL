fd351b2b06976e013a40d4ec768cf8f3de3e4bfd
Harald Kleinschrot Colomer <kcharald@gmail.com>
2025-12-18 16:27:00 +0100

    esqueleto importado xd

--- FIREBALL/Assets/_MonsterBattleSandbox/_Scripts/Combat/IAttackable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IAttackable
-{
-    GameObject gameObject { get; }
-    void OnAttack(GameObject attacker, Attack attack);
-}
--- FIREBALL/Assets/_MonsterBattleSandbox/_Scripts/_Monster/ScriptableObjectAI/MonsterActions/MonsterAction.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class MonsterAction : ScriptableObject
-{
-    public abstract void MonsterAct(MonsterStateController monsterController);
-}
