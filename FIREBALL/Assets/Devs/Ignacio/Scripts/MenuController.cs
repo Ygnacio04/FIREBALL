94a593cf0c47d73f029b3bf4bd7b40675e417677
Ygnacio04 <i20ag04@gmail.com>
2026-01-12 11:56:05 +0100

    Preubas SceneController

--- FIREBALL/Assets/Devs/Ignacio/Scripts/MenuController.cs
+++ FIREBALL/Assets/Devs/Ignacio/Scripts/MenuController.cs
@@ -7,6 +7,7 @@ public class MenuController : MonoBehaviour
 {
     [SerializeField] private GameObject MainCanvas, OptionsCanvas;
     [SerializeField] private Slider AmbienceSlider, MusicSlider, FXSlider;
+    [SerializeField] private SceneController sceneController;
 
     void Start()
     {
@@ -21,7 +22,6 @@ public class MenuController : MonoBehaviour
 
         OnMusicVolumeChanged();  
         OnAmbienceVolumeChanged(); 
-
     }
 
     public void MostrarPanelMenuPrincipal()
@@ -38,30 +38,53 @@ public class MenuController : MonoBehaviour
 
     public void Salir()
     {
-        Application.Quit();
+        if (sceneController != null)
+        {
+            sceneController.QuitApplication();
+        }
+        else
+        {
+            Application.Quit();
+        }
     }
 
     public void IniciarMainGame()
     {
-        //GameSettings.CurrentGameMode = "MainGame"; 
-
+        // Por ahora carga la escena de créditos como prueba
+        if (sceneController != null)
+        {
+            sceneController.LoadCreditosScene();
+        }
+        else
+        {
+            SceneManager.LoadScene("Creditos");
+        }
     }
 
     public void OnAmbienceVolumeChanged()
     {
-        SoundManager.Instance.SetAmbienceVolume(AmbienceSlider.value);
-        PlayerPrefs.SetFloat("AmbienceVolume", AmbienceSlider.value); 
+        if (SoundManager.Instance != null)
+        {
+            SoundManager.Instance.SetAmbienceVolume(AmbienceSlider.value);
+            PlayerPrefs.SetFloat("AmbienceVolume", AmbienceSlider.value);
+        }
     }
 
     public void OnMusicVolumeChanged()
     {
-        SoundManager.Instance.SetMusicVolume(MusicSlider.value);
-        PlayerPrefs.SetFloat("MusicVolume", MusicSlider.value);
+        if (SoundManager.Instance != null)
+        {
+            SoundManager.Instance.SetMusicVolume(MusicSlider.value);
+            PlayerPrefs.SetFloat("MusicVolume", MusicSlider.value);
+        }
     }
 
     public void OnFXVolumeChanged()
     {
-        SoundManager.Instance.SetFxVolume(FXSlider.value);
-        PlayerPrefs.SetFloat("FXVolume", FXSlider.value); 
+        if (SoundManager.Instance != null)
+        {
+            SoundManager.Instance.SetFxVolume(FXSlider.value);
+            PlayerPrefs.SetFloat("FXVolume", FXSlider.value);
+        }
     }
 }
